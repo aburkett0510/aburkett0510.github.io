278233b04df45f4c66bff85e7ec84269d710565f
Austin-PocketBard <aburkett0510@gmail.com>
2024-12-11 17:00:56 -0500

    Upload revision

--- TheYard/script.js
+++ TheYard/script.js
@@ -14,11 +14,11 @@ document.addEventListener("DOMContentLoaded", function() {
         document.querySelector('#text').src = textElement[randomIndex];
     });
 
-    function handleFiles(event) {
+    /*function handleFiles(event) {
         var files = event.target.files;
         $("#src").attr("src", URL.createObjectURL(files[0]));
         document.getElementById("audio").load();
     }
     
-    document.getElementById("upload").addEventListener("change", handleFiles, false);
+    document.getElementById("upload").addEventListener("change", handleFiles, false);*/
 });
